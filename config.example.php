8e53d7ee38bbedbca2a8d7080a97504379dbeb94
Jared Howland <github@jaredhowland.com>
2013-02-25 21:04:28 -0700

    Uploaded example config file.

---
+++ config.example.php
@@ -0,0 +1,69 @@
+<?php
+/**
+  * Configuration class.
+  *
+  * @author Jared Howland <marc.records@jaredhowland.com>
+  * @version 2013-02-25
+  * @since 2013-02-22
+  *
+  */
+
+class config {
+  // Database settings
+  const DB_HOST          = '';
+  const DB_PORT          = '';
+  const DB_NAME          = '';
+  const DB_USERNAME      = '';
+  const DB_PASSWORD      = '';
+  // App settings
+  const DEVELOPMENT      = TRUE; // Changes the app behavior (error reporting, template caching, which database to use, etc.)
+  const URL              = '';
+  const UPLOAD_DIRECTORY = '';
+  const TIME_ZONE        = ''; // Needed for date calculations in PHP
+  const NOTIFY_EMAILS    = '';
+
+
+/****************************************************************************/
+/*                       DO NOT EDIT BELOW THIS LINE                        */
+/****************************************************************************/
+
+  /**
+    * Determines type of error reporting
+    * Based on state of DEVELOPMENT constant
+    *
+    * @param null
+    * @return string Type of error reporting
+    */
+  public static function set_error_reporting() {
+    if(self::DEVELOPMENT) {
+      // error_reporting(E_ALL);
+      ini_set('error_reporting', E_ALL^E_NOTICE);
+      ini_set('display_errors', 1);
+    } else {
+      error_reporting(0);
+    }
+  }
+  
+} // End class
+
+
+/****************************************/
+/* Miscellaneous configuration settings */
+/****************************************/
+
+// Autoload classes
+// Must be in the 'classes' directory and prefixed with 'class.'
+function __autoload( $class ) {
+  require_once( __DIR__ . '/classes/class.' . $class . '.php' );
+}
+
+// Set default time zone
+date_default_timezone_set( config::TIME_ZONE );
+
+// Increase memory limit for large reading schedules
+ini_set('memory_limit', '128M');
+
+// Set error reporting
+config::set_error_reporting();
+
+?>
