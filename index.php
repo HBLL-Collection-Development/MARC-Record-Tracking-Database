11dee625d64b202718ec8a3338f219640583edd4
Jared Howland <jared_howland@byu.edu>
2013-03-03 21:00:21 -0700

    Added statistics to main page.

--- index.php
+++ index.php
@@ -9,6 +9,21 @@ $query->execute();
 $marc_record_loads = $query->fetchAll(PDO::FETCH_ASSOC);
 $db = null;
 
-$html = array('title' => 'Home', 'results' => $marc_record_loads);
+$num_resources            = count($marc_record_loads);
+$num_up_to_date_resources = 0;
+$num_records              = 0;
+
+foreach($marc_record_loads as $resource) {
+  $num_records += $resource['num_records'];
+  // To count as being up to date, must have a file associated with it, and have the next load date in the future
+  if($resource['file_exists'] == 'Y' && (strtotime($resource['next_load']) > strtotime(date('Y-m-d')) || is_null($resource['next_load']))) {
+	$num_up_to_date_resources++;
+  }
+}
+$percent_complete = round(($num_up_to_date_resources / $num_resources) * 100) . '%';
+
+$stats = array('num_records' => $num_records, 'num_resources' => $num_resources, 'percent_complete' => $percent_complete);
+
+$html = array('title' => 'Home', 'stats' => $stats, 'results' => $marc_record_loads);
 template::display('html.tmpl', $html);
 ?>
